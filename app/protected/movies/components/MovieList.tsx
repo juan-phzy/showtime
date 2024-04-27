1608e76ad9c43c0c2457af626d4c0b2fe0caf105
Elijah <ewerselijah@gmail.com>
2024-04-26 20:27:43 -0400

    Styled the movies page for mobile and web

--- app/protected/movies/components/MovieList.tsx
+++ app/protected/movies/components/MovieList.tsx
@@ -11,44 +11,39 @@ interface Props{
 
 export const MovieList = ({moviesNowShowing,moviesComingUp}: Props) => {
   
-    
-    useEffect(() => {
-        document.body.style.overflowY = "auto"
-    });
-
     const [nowPlaying,setNowPlaying] = useState<boolean>(true)
 
     return (
         <section className = "movies-page-container">
-        <div className = "movies-pages-content">
-        <div className = "movies-page-header mb-4">
-            <button className={`flex justify-center items-center w-fit h-fit border-solid border-white border-2 ${nowPlaying && "bg-gray-500"}`}
-            onClick={() =>   
-            setNowPlaying(true)}>
-            Now Playing
-            </button>
-            <button
-            className={`flex justify-center items-center w-fit h-fit border-solid border-white border-2 ${!nowPlaying && "bg-gray-500"}`}
-            onClick={() => setNowPlaying(false)}>
-            Coming Soon
-            </button>
-        </div>
-        <div className = "movie-card w-full flex flex-wrap"> 
-            { (nowPlaying? moviesNowShowing: moviesComingUp).map(
-                
-                (movie:MovieGluFilm, index) => { return ( 
-                <button key={index} className="border-solid border-white border-2 w-1/2 hover:bg-white/60"><MovieCard
-                image = {movie.images.poster['1'].medium.film_image}
-                name = {movie.film_name}
-                rating = {""}
-                watchtime = {""}
-                genre = {""}/>
-                </button> ) }  
-                
-            )}      
+            <div className = "movies-pages-content">
+                <div className = "movies-page-header mb-4">
+                    <button className={`flex justify-center items-center w-fit h-fit border-solid border-white border-2 ${nowPlaying && "bg-gray-500"}`}
+                    onClick={() =>   
+                    setNowPlaying(true)}>
+                        Now Playing
+                    </button>
+                    <button
+                    className={`flex justify-center items-center w-fit h-fit border-solid border-white border-2 ${!nowPlaying && "bg-gray-500"}`}
+                    onClick={() => setNowPlaying(false)}>
+                        Coming Soon
+                    </button>
+                </div>
+                <div className = "movie-card-container w-full flex flex-wrap"> 
+                    { (nowPlaying? moviesNowShowing: moviesComingUp).map(
+                        
+                        (movie:MovieGluFilm, index) => { return ( 
+                        <button key={index} className="movie-card"><MovieCard
+                        image = {movie.images.poster['1'].medium.film_image}
+                        name = {movie.film_name}
+                        rating = {""}
+                        watchtime = {""}
+                        genre = {""}/>
+                        </button> ) }  
+                        
+                        )}      
 
-        </div>
-        </div>
-    </section>
+                </div>
+            </div>
+        </section>
     )
 }
